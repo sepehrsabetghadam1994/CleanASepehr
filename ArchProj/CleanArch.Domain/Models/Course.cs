bc3ae526bcd838684f247e6b69db8e95e63f97b3
Sepehr Sabetghadam <sepehr.sabetghadam@gmail.com>
2020-12-07 01:20:30 +0330

    create Course Model

---
+++ ArchProj/CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CleanArch.Domain.Models
+{
+   public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
