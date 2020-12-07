f616577f59ed1f7ca78afc5effbad847a921943f
Sepehr Sabetghadam <sepehr.sabetghadam@gmail.com>
2020-12-07 19:38:53 +0330

    Create Service And Repository

---
+++ ArchProj/Arch.Application/Services/CoursesService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Intefaces;
+
+namespace CleanArch.Application.Services
+{
+    public class CoursesService : ICoursesService
+    {
+        private ICoursesRepository _courseRepository;
+        public CoursesService(ICoursesRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public CoursesViewModel GetCourses()
+        {
+            return new CoursesViewModel()
+            {
+                Courses= _courseRepository.GetCourses()
+
+            };
+                
+        }
+    }
+}
---
+++ ArchProj/CleanArch.Infra.Datas/Repository/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Intefaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICoursesRepository
+    {
+        private UnivercityDBContext _db;
+        public CourseRepository(UnivercityDBContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _db.Courses;
+        }
+    }
+}
