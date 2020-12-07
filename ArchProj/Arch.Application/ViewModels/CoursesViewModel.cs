679cf2adcfa0ae2efcaa2ba11cd25362ebe7f282
Sepehr Sabetghadam <sepehr.sabetghadam@gmail.com>
2020-12-07 19:07:21 +0330

    Initial application

---
+++ ArchProj/Arch.Application/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Application.ViewModels
+{
+   public class CoursesViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
+    
---
+++ ArchProj/CleanArch.Domains/Intefaces/ICoursesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Domain.Intefaces
+{
+   public interface ICoursesRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
